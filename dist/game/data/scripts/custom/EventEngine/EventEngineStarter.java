125818618bb8187be5b5fb03eba7bf13e44dc4d3
Lucas <luksdlt92@gmail.com>
2017-12-08 20:05:11 -0300

    Moving AthenaEngine files

--- dist/game/data/scripts/custom/EventEngine/EventEngineStarter.java
+++ dist/game/data/scripts/custom/EventEngine/EventEngineStarter.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package custom.events.EventEngine;
+package custom.EventEngine;
 
 import com.github.athenaengine.core.EventEngineManager;
 import ai.npc.AbstractNpcAI;
